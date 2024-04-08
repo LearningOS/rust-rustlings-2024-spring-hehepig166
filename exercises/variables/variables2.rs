062bd940a729d6a14c4742ebe81fa1e9187f74fc
hehepig166 <1260647889@qq.com>
2024-04-08 12:57:07 +0800

    update

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,10 +3,19 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+use std::any::{type_name, TypeId};
+ 
+fn print_type_of<T>(_: &T) {
+    println!("{}", type_name::<T>());
+}
 
 fn main() {
-    let x;
+    let mut x : i64 = 10;
+    print_type_of(&x);
+    print_type_of(&10);
+    print_type_of(&2147483648u32);
     if x == 10 {
         println!("x is ten!");
     } else {
