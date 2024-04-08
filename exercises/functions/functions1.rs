289ae39fa377346bbe115b64bb995931df027fef
hehepig166 <1260647889@qq.com>
2024-04-08 13:03:29 +0800

    update

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,7 +3,11 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+fn call_me() {
+    // return ();
+}
 
 fn main() {
     call_me();
--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -2,13 +2,19 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
+    // If both numbers are equal, any of them can be returned.
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 // Don't mind this for now :)
@@ -24,5 +30,10 @@ mod tests {
     #[test]
     fn fortytwo_is_bigger_than_thirtytwo() {
         assert_eq!(42, bigger(32, 42));
+    }
+
+    #[test]
+    fn equal_numbers() {
+        assert_eq!(42, bigger(42, 42));
     }
 }
